b8b813f22ed04500e331422edac83ebb450a31a4
JuzuSss <99507916+JuzuSss@users.noreply.github.com>
2024-03-18 02:35:52 +0100

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,35 @@
+function scrollFunction() {
+    var windowHeight = window.innerHeight;
+
+    var elementresume = document.getElementById("hiddenText");
+    var elementPositionresume = elementresume.getBoundingClientRect().top;
+    if (elementPositionresume < windowHeight) {
+      elementresume.classList.add("visible");
+    } else {
+      elementresume.classList.remove("visible");
+    }
+
+
+    for (var i = 0; i <= 22; i++) {
+        var elementLink = document.getElementById("hiddenText" + i);
+        var elementPositionLink = elementLink.getBoundingClientRect().top;
+    
+        if (elementPositionLink < windowHeight) {
+            elementLink.classList.add("visible" + i); 
+         } else {
+                elementLink.classList.remove("visible" + i); 
+        }
+    }
+    
+
+    
+
+}
+
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+
+scrollFunction();
