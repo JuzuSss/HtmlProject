b66e402701185dfe69e47400bbac75eadf6cab30
Julien <julien.wan@epita.fr>
2024-06-21 06:41:29 +0200

    oiowf

--- script7.js
+++ script7.js
@@ -16,14 +16,16 @@ function scrollFunction() {
     var windowHeight = window.innerHeight;
     
     for (var i = 1; i <= 43; i++) {
-        var elementLink = document.getElementById("hiddenText" + i);
-        var elementPositionLink = elementLink.getBoundingClientRect().top;
-    
-        if (elementPositionLink < windowHeight) {
-            elementLink.classList.add("visible" + i); 
-         } else {
-                elementLink.classList.remove("visible" + i); 
-        }
+      if (i == 8 || i == 34 || i == 35 || i == 36 || i == 37 || i == 38){
+          continue
+      }
+      var elementLink = document.getElementById("hiddenText" + i);
+      var elementPositionLink = elementLink.getBoundingClientRect().top;
+      if (elementPositionLink < windowHeight) {
+          elementLink.classList.add("visible" + i); 
+        } else {
+              elementLink.classList.remove("visible" + i); 
+      }
     }
   
     
